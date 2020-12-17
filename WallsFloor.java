ecb8bb510090afc2659e7b0f4aa717eaab8460f0
grigoriyfedyakov <74509979+grigoriyfedyakov@users.noreply.github.com>
2020-12-17 17:21:04 +0300

    Add files via upload

---
+++ WallsFloor.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+public class WallsFloor extends Cave{
+    private String name;
+    public WallsFloor(String name) {
+        super(name);
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void facing(String str) {
+        System.out.print(name+" облицовывались "+ str);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        WallsFloor that = (WallsFloor) o;
+        return Objects.equals(name, that.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
+    }
+
+    public String toString() {
+        return "WallsFloor{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
